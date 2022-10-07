8a1660250cd15a77d380e984fe8eb7264477e41c
Juliette Cordor <grassojames5@gmail.com>
2022-10-07 18:10:03 +1100

    fixed bug

--- src/color.rs
+++ src/color.rs
@@ -26,14 +26,14 @@ fn validate_hex_len(len: usize) -> Result<(), ColorError> {
         return Err(ColorError::IncludesAlpha);
     }
 
-    if len != 3 || len != 6 {
+    if len != 3 && len != 6 {
         return Err(ColorError::InvalidHexLength);
     }
 
     Ok(())
 }
 
-fn hex_3_to_6<'a>(hex_trois: &str) -> String {
+fn hex_3_to_6(hex_trois: &str) -> String {
     let mut hex = String::new();
 
     for c in hex_trois.chars() {
@@ -76,5 +76,13 @@ mod tests {
     #[test]
     fn test_parse_hex() {
         assert_eq!(parse_hex("#fff"), Ok(Color::Yellow));
+    }
+
+    #[test]
+    fn test_hex_to_digit() {
+        let hex = 'f';
+        let digit = hex_char_to_digit(hex);
+
+        assert_eq!(digit, 15);
     }
 }
