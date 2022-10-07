de81425e61907b0b1460aeccf3cf3b1f76cec505
Juliette Cordor <grassojames5@gmail.com>
2022-10-07 18:00:08 +1100

    fixed validation

--- src/color.rs
+++ src/color.rs
@@ -9,28 +9,44 @@ pub enum ColorError {
     InvalidHex,
     #[error("Hex value includes alpha channel")]
     IncludesAlpha,
+    #[error("Hex value must be 3 or 6 characters")]
+    InvalidHexLength,
 }
 
-fn validate_chars(chars: Chars) -> bool {
-    chars.all(|c| c.is_ascii_hexdigit())
+fn validate_chars(chars: Chars) -> Result<(), ColorError> {
+    if chars.all(|c| c.is_ascii_hexdigit()) {
+        Ok(())
+    } else {
+        Err(ColorError::InvalidHex)
+    }
+}
+
+fn validate_hex_len(len: usize) -> Result<(), ColorError> {
+    if len == 4 || len == 8 {
+        return Err(ColorError::IncludesAlpha);
+    }
+
+    if len != 3 || len != 6 {
+        return Err(ColorError::InvalidHexLength);
+    }
+
+    Ok(())
 }
 
 pub fn parse_hex(raw_hex: impl AsRef<str>) -> Result<Color, ColorError> {
     let raw_hex = raw_hex.as_ref();
 
-    if !raw_hex.starts_with("#") {
-        return Err(ColorError::InvalidHex);
-    }
+    let hex_value = if raw_hex.starts_with("#") {
+        raw_hex.trim_start_matches("#")
+    } else {
+        raw_hex
+    };
 
-    let hex_value = raw_hex.trim_start_matches("#");
-
-    validate_chars(hex_value.chars());
+    validate_chars(hex_value.chars())?;
 
     let hex_value_len = hex_value.len();
 
-    if hex_value_len == 4 || hex_value_len == 8 {
-        return Err(ColorError::IncludesAlpha);
-    }
+    validate_hex_len(hex_value_len)?;
 
     // TMP
     Ok(Color::Yellow)
