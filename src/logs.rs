bcdb42380e4868b2bfd03d17b9be8106d3c84a0d
Juliette Cordor <grassojames5@gmail.com>
2022-10-12 10:13:43 +1100

    fixes

--- src/logs.rs
+++ src/logs.rs
@@ -23,28 +23,35 @@ fn get_log_path() -> std::io::Result<PathBuf> {
     Ok(base_path)
 }
 
+#[cfg(not(profile = "release"))]
 static TRACING_GUARD: Mutex<Option<WorkerGuard>> = Mutex::new(None);
 
 pub fn init_tracing() -> anyhow::Result<()> {
-    let mut path = get_log_path()?;
-    let file_name = chrono::Local::now()
-        .format("dir-kill.%Y-%m-%d_%H-%M-%S.log")
-        .to_string();
+    cfg_if::cfg_if! {
+        if #[cfg(profile = "release")] {
+            Ok(())
+        } else {
+            let mut path = get_log_path()?;
+            let file_name = chrono::Local::now()
+                .format("dir-kill.%Y-%m-%d_%H-%M-%S.log")
+                .to_string();
 
-    path.push(file_name);
+            path.push(file_name);
 
-    let file = File::create(path)?;
+            let file = File::create(path)?;
 
-    let (non_blocking, guard) = tracing_appender::non_blocking(Writer::new(file));
+            let (non_blocking, guard) = tracing_appender::non_blocking(Writer::new(file));
 
-    tracing_subscriber::fmt()
-        .with_span_events(FmtSpan::NEW | FmtSpan::CLOSE | FmtSpan::ENTER | FmtSpan::EXIT)
-        .with_thread_names(true)
-        .with_max_level(Level::DEBUG)
-        .with_writer(non_blocking)
-        .init();
+            tracing_subscriber::fmt()
+                .with_span_events(FmtSpan::NEW | FmtSpan::CLOSE | FmtSpan::ENTER | FmtSpan::EXIT)
+                .with_thread_names(true)
+                .with_max_level(Level::DEBUG)
+                .with_writer(non_blocking)
+                .init();
 
-    *TRACING_GUARD.lock() = Some(guard);
+            *TRACING_GUARD.lock() = Some(guard);
 
-    Ok(())
+            Ok(())
+        }
+    }
 }
